c813d6c6b9e321a70ebf030a841ae442a9c36b11
itLead797 <rob.price.home@gmail.com>
2017-10-15 12:52:28 -0400

    removal of gallery images from assets directory. Update of fileservice data

--- src/app/file.service.data.ts
+++ src/app/file.service.data.ts
@@ -1,84 +1,85 @@
 // ../assets/img/
 
+export const BASEURL = '../dist/assets/img/';
 
 export const IMAGES: any = [
   {
-    'url': '../assets/img/IMG_1411.JPG',
+    'url': BASEURL + 'IMG_1411.JPG',
     'title': 'Fall Leaf',
     'caption': 'Leaf in the fall'
   },
   {
-    'url': '../assets/img/img_1467.jpg',
+    'url': BASEURL + 'img_1467.jpg',
     'title': 'Birds',
     'caption': 'Fall birds in a tree'
   },
   {
-    'url': '../assets/img/IMG_1475.JPG',
+    'url': BASEURL + 'img/IMG_1475.JPG',
     'title': 'Walking',
     'caption': 'Family walk in the park'
   },
   {
-    'url': '../assets/img/IMG_1479.JPG',
+    'url': BASEURL + 'IMG_1479.JPG',
     'title': 'Water',
     'caption': 'Cool fall stream'
   },
   {
-    'url': '../assets/img/IMG_3115.a.JPG',
+    'url': BASEURL + 'IMG_3115.a.JPG',
     'title': 'Fall Bird',
     'caption': 'Colorful fall bird'
   },
   {
-    'url': '../assets/img/IMG_3163.JPG',
+    'url': BASEURL + 'img/IMG_3163.JPG',
     'title': 'Path',
     'caption': 'Path in the fall'
   },
   {
-    'url': '../assets/img/IMG_3235.1.jpg',
+    'url': BASEURL + 'img/IMG_3235.1.jpg',
     'title': 'Fence',
     'caption': 'Webs in the wind'
   },
   {
-    'url': '../assets/img/IMG_3247.1.jpg',
+    'url': BASEURL + 'IMG_3247.1.jpg',
     'title': 'Pumpkin',
     'caption': 'Porch pumpkin'
   },
   {
-    'url': '../assets/img/IMG_3248.1.jpg',
+    'url': BASEURL + 'img/IMG_3248.1.jpg',
     'title': 'Sunset',
     'caption': 'Sunset over the roof tops'
   },
   {
-    'url': '../assets/img/IMG_3253.1.jpg',
+    'url': BASEURL + 'img/IMG_3253.1.jpg',
     'title': 'Backyard',
     'caption': 'Fall colors in the backyard'
   },
   {
-    'url': '../assets/img/IMG_3265.1.jpg',
+    'url': BASEURL + 'img/IMG_3265.1.jpg',
     'title': 'Moon',
     'caption': 'Cool moonlight'
   },
   {
-    'url': '../assets/img/img_1424.jpg',
+    'url': BASEURL + 'img/img_1424.jpg',
     'title': 'Barn',
     'caption': 'Abandoned barn'
   },
   {
-    'url': '../assets/img/IMG_2644.JPG',
+    'url': BASEURL + 'img/IMG_2644.JPG',
     'title': 'Fancy Celling',
     'caption': 'Stairwell at old Coca-Cola plant'
   },
   {
-    'url': '../assets/img/IMG_0284.JPG',
+    'url': BASEURL + 'img/IMG_0284.JPG',
     'title': 'Balloons',
     'caption': 'Avon hot air balloon festival'
   },
   {
-    'url': '../assets/img/IMG_3246.1.jpg',
+    'url': BASEURL + 'IMG_3246.1.jpg',
     'title': 'Girl in Chair',
     'caption': 'Spectator at horse jumping'
   },
   {
-    'url': '../assets/img/IMG_1074.1.jpg',
+    'url': BASEURL + 'IMG_1074.1.jpg',
     'title': 'Gun tricks',
     'caption': 'Wild west festival'
   }
