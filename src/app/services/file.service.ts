4200f02f858e8e0334917b01e57f3db31ad74963
Rob Price <rob.price@libertymutual.com>
2017-11-06 14:38:05 -0500

    fix to karma for cli v5

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -5,10 +5,12 @@ import { IMAGES } from './file.service.data';
 @Injectable()
 export class FileService {
  images: ImageRs[];
+  __dirname = '../../../img';
   constructor() { }
 
   public getFiles(): ImageRs[] {
     this.images = IMAGES;
+   // this.getFileList();
       return this.images;
   }
 
@@ -17,8 +19,23 @@ export class FileService {
     let fileList = [];
     const fse = require('fs-extra');
 
-    let files = fse.readdirSync('../../../img/');
-    console.log(files);
+    let fs = require('fs');
+    fs.realPath(__dirname, function(err, path) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Path is: ' + path);
+    });
+    fs.readdir(__dirname, function(err, files) {
+      if (err) { return; }
+      files.forEach(function(f) {
+        console.log('Files: ' + f);
+      });
+    });
+
+    // let files = fse.readdirSync('../../../img/');
+    // console.log(files);
   }
 }
 
